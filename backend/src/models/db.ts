4f1a8d5d4ac5904f0cb5e9c49167f2ae4861a572
ayushkrishnan <114354841+ayushkrishnan@users.noreply.github.com>
2024-12-24 12:41:39 +0530

    error resolve

--- backend/src/models/db.ts
+++ backend/src/models/db.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 
-const db = drizzle(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@db/smarthire`);
+const db = drizzle(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost/smarthire`);
 
 export default db;
