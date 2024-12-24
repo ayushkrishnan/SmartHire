e513a653151ad33350982d50a973fb264461e4fc
ayushkrishnan <ayushkrishnanu2025@cs.sjcetpalai.ac.in>
2024-12-24 12:39:21 +0530

    fix cors for chrome

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import userRouter from "./routes/users";
 import jobRouter from "./routes/jobs";
 import applicationRouter from "./routes/applications";
 
 const app = express();
+app.use(cors({
+    origin: ["http://localhost:4173", "http://localhost:5173"],
+    credentials: true
+}))
+
 app.use(express.json());
 app.use(cookieParser());
 
