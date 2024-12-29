6c7f92fd1a101f1d8de1d048052e2c36c5c130f4
ayushkrishnan <ayushkrishnanu2025@cs.sjcetpalai.ac.in>
2024-12-29 13:08:59 +0530

    change to host.docker.internal

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   dialect: 'postgresql', // 'mysql' | 'sqlite' | 'turso'
   schema: './src/models/schema.ts',
   dbCredentials: {
-    host: "db",
+    host: "host.docker.internal",
     database: "smarthire",
     user: "postgres",
     password: "password",
